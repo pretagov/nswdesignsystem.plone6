12c076d952b0fc177504030be6f7e6686d630714
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2022-07-11 17:10:18 +0100

    Unused files cleanup

--- setup.py
+++ setup.py
@@ -22,7 +22,6 @@ setup(
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: Addon",
-        "Framework :: Plone :: 5.2",
         "Framework :: Plone :: 6.0",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.7",
