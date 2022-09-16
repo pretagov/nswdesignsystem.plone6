aca3da2ca8d440099e20bd6967073d29383353a1
Jefferson Bledsoe <me@jeffersonbledsoe.com>
2022-09-16 12:39:49 +0000

    Search page setup (#1)
    * Create Search content page
    
    * Check to see if search page exists
    
    * Set facet title
    
    * Don't show Image or File types in search
    
    * Exclude page from global nav

--- src/nswdesignsystem/plone6/setuphandlers.py
+++ src/nswdesignsystem/plone6/setuphandlers.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+from plone.api.content import create as createContent
+from plone.api.content import get as getContent
+from plone.api.content import delete as deleteContent
+from plone.api.portal import get as getPortal
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
 
@@ -16,11 +20,59 @@ class HiddenProfiles(object):
         return ["nswdesignsystem.plone6.upgrades"]
 
 
+def create_global_search_page(context):
+    portal = getPortal()
+    search_page = getContent(path="/about")
+
+    if not search_page:
+        search_page = createContent(
+            id="search", type="Document", title="Search", container=portal
+        )
+    search_page.blocks = {
+        "cc6cf326-69b5-42da-b768-0eb89b0a152f": {"@type": "title"},
+        "e633675a-8067-49cb-97c5-b74ac7314f53": {
+            "@type": "search",
+            "showSearchInput": True,
+            "facetsTitle": "Filter results",
+            "listingBodyTemplate": "default",
+            "showSortOn": True,
+            "sortOnLabel": "Sort by",
+            "sortOnOptions": ["created", "sortable_title"],
+            "variation": "facetsLeftSide",
+            "query": {
+                "query": [
+                    {
+                        "i": "path",
+                        "o": "plone.app.querystring.operation.string.absolutePath",
+                        "v": "/",
+                    },
+                    {
+                        "i": "portal_type",
+                        "o": "plone.app.querystring.operation.selection.none",
+                        "v": ["Image", "File"],
+                    },
+                ],
+                "sort_order": "ascending",
+            },
+        },
+    }
+    search_page.blocks_layout = {
+        "items": [
+            "cc6cf326-69b5-42da-b768-0eb89b0a152f",
+            "e633675a-8067-49cb-97c5-b74ac7314f53",
+        ]
+    }
+    search_page.exclude_from_nav = True
+
+
 def post_install(context):
     """Post install script"""
+    create_global_search_page(context)
     # Do something at the end of the installation of this package.
 
 
 def uninstall(context):
     """Uninstall script"""
+    portal = getPortal()
+    deleteContent(portal["search"])
     # Do something at the end of the uninstallation of this package.
