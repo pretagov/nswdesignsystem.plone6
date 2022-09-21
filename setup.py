0802923b5294aae96f34ac25cb5b0aea720a0c56
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2022-09-21 16:03:36 +0100

    Back to development: 0.1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = "\n\n".join(
 
 setup(
     name="nswdesignsystem.plone6",
-    version="0.1.0",
+    version="0.1.1.dev0",
     description="Plone backend package for the NSW Design system",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
