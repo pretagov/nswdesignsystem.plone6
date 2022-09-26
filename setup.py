867ebdaf90e09e92764661c3f477fedca7da3f22
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2022-09-26 14:44:14 +0100

    Remove Python3.7 support

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
         "Framework :: Plone :: Addon",
         "Framework :: Plone :: 6.0",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
