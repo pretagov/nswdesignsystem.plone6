ae9da0a2279cba26ea0cf59e0f6defe660115150
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2022-10-17 15:31:13 +0100

    Initial REST API for nswSiteSetting

--- src/nswdesignsystem/plone6/interfaces.py
+++ src/nswdesignsystem/plone6/interfaces.py
@@ -1,8 +1,84 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
+from plone.autoform import directives
+from plone.base.interfaces.controlpanel import ISocialMediaSchema
+from plone.schema import ASCIILine, Bool, Int
+from plone.supermodel.interfaces import FIELDSETS_KEY
+from plone.supermodel.model import Fieldset
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from zope.schema import getFields
+
+from nswdesignsystem.plone6.constants import (
+    COLOUR_PALETTE,
+)
 
 
 class INswdesignsystemPlone6Layer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+
+class INSWDesignSystemSettings(ISocialMediaSchema):
+    directives.omitted("share_social_data")
+    directives.omitted("facebook_app_id")
+    breadcrumb_start_depth = Int(
+        title="Breadcrumb start depth",
+        description="What depth in the site to start showing the breadcrumb",
+        min=1,
+        max=5,
+        default=2,
+    )
+    show_site_title_text = Bool(
+        title="Show site title name in header",
+        description="Whether to show the name of the site next to the site logo",
+        default=True,
+    )
+    linkedin_url = ASCIILine(
+        title="LinkedIn URL",
+        required=False,
+    )
+    youtube_url = ASCIILine(
+        title="YouTube URL",
+        required=False,
+    )
+    nsw_brand_dark = ASCIILine(
+        title="NSW Brand dark colour",
+        description="Set the primary colour",
+        required=False,
+    )
+    directives.widget(
+        "nsw_brand_dark",
+        frontendOptions={
+            "widget": "color_picker",
+            "widgetProps": {"colors": COLOUR_PALETTE},
+        },
+    )
+
+
+excluded_social_fields = ["share_social_data", "facebook_app_id"]
+social_fields = [
+    *[
+        field
+        for field in getFields(ISocialMediaSchema).keys()
+        if field not in excluded_social_fields
+    ],
+    "linkedin_url",
+    "youtube_url",
+]
+
+INSWDesignSystemSettings.setTaggedValue(
+    FIELDSETS_KEY,
+    [
+        Fieldset(
+            "social_fieldset",
+            fields=social_fields,
+            label="Social links",
+        ),
+        Fieldset(
+            "independent_fieldset",
+            fields=["show_site_title_text", "nsw_brand_dark"],
+            label="Independent branding",
+            description="Settings in here require an independent branding exemption.",
+        ),
+    ],
+)
---
+++ src/nswdesignsystem/plone6/restapi/get.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from nswdesignsystem.plone6.interfaces import INSWDesignSystemSettings
+from nswdesignsystem.plone6.restapi.serializer.nsw_site_settings import serialize_data
+from plone import api
+from plone.autoform.interfaces import OMITTED_KEY
+from plone.restapi.services import Service
+from zope.i18n import translate
+from zope.interface import implementer
+from zope.publisher.interfaces import IPublishTraverse
+from zope.schema import getFields
+
+
+@implementer(IPublishTraverse)
+class NSWSiteSettingsGet(Service):
+    def __init__(self, context, request):
+        super(NSWSiteSettingsGet, self).__init__(context, request)
+
+    def reply(self):
+        omitted_fields = INSWDesignSystemSettings.getTaggedValue(OMITTED_KEY)
+        # Using the fieldset key to ensure that the removed fields from ISocialMediaSchema aren't included
+        fields = [
+            field
+            for field in getFields(INSWDesignSystemSettings)
+            if field not in omitted_fields
+        ]
+
+        try:
+            records = {
+                field: api.portal.get_registry_record(
+                    # TODO: Why do I need to manually add the prefix here?
+                    f"nswdesignsystem.{field}",
+                    interface=INSWDesignSystemSettings,
+                    default="",
+                )
+                for field in fields
+            }
+        except KeyError as e:
+            # TODO: Better logging & response of missing key
+            return {}
+        except Exception as e:
+            # TODO: Do I need a 400 error?
+            self.request.response.setStatus(400)
+            return {
+                "error": dict(
+                    type=e.__class__.__name__,
+                    message=translate(str(e), context=self.request),
+                )
+            }
+
+        if not records:
+            return {}
+
+        return serialize_data(json_data=records)
