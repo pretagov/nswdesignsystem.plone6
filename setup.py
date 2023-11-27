2d5786c10f108eae7783242f60cb6200106fe884
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2023-11-27 18:11:50 +0000

    Remove python 3.8

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
         "Framework :: Plone :: Addon",
         "Framework :: Plone :: 6.0",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
