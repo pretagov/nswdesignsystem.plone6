920a2873abd799eeda848dcb825ae0d13e32341e
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2026-06-04 18:09:16 +0100

    Bump supported python version

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.9, <3.12",
+    python_requires=">=3.9, <3.13",
     install_requires=[
         "setuptools",
         # -*- Extra requirements: -*-
