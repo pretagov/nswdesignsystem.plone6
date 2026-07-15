983c60f2976cce9e8d4a9c6f8e78fea16605235b
Jefferson Bledsoe <30210785+JeffersonBledsoe@users.noreply.github.com>
2026-07-15 15:15:58 +0000

    Update tooling (#26)
    * Bump supported python version
    
    * Update other tooling

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
@@ -48,7 +49,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.9, <3.12",
+    python_requires=">=3.9, <3.13",
     install_requires=[
         "setuptools",
         # -*- Extra requirements: -*-
